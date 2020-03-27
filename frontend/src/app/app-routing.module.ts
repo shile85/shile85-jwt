71bed68e7de55f4132fed84ba19753b452ce6237
Vladimir Pesic <ios@vladimirpesic.rs>
2020-03-27 14:43:16 +0100

    Singup component and routing added to frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'singup',
+    component: SingupComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'request-password',
+    component: RequestResetComponent
+  },
+  {
+    path: 'response-password',
+    component: ResponseResetComponent
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
 import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { SingupComponent } from './components/singup/singup.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ResponseResetComponent } from './components/password/response-reset/res
     LoginComponent,
     ProfileComponent,
     RequestResetComponent,
-    ResponseResetComponent
+    ResponseResetComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
